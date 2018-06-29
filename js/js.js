2d762f5ee4a27cba7acb37fbf66ebba0b5c71eef
Jimmy <602510699@qq.com>
2018-06-29 11:24:41 +0800

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,71 @@
+﻿// JavaScript Document
+$(function () {
+	
+	
+	//内容页左边导航
+    $(".float_nav dl").click(function () {
+        $(this).addClass("selected")
+          .children("dd").show(1000).end()
+          .siblings().removeClass("selected")
+          .children("dd").hide(1000); 
+    });
+	
+	$(".float_nav dl dd a").click(function(){
+		$(this).addClass("a_selected").parent().siblings().find("a").removeClass("a_selected")
+		}
+	)
+	
+	
+		 //toTop返回顶部
+    	// browser window scroll (in pixels) after which the "back to top" link is shown
+	var offset = 300,
+		//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+		offset_opacity = 1200,
+		//duration of the top scrolling animation (in ms)
+		scroll_top_duration = 700,
+		//grab the "back to top" link
+		$back_to_top = $('.toTop');
+
+	//hide or show the "back to top" link
+	$(window).scroll(function(){
+		( $(this).scrollTop() > offset ) ? $back_to_top.addClass('toTop_visible') : $back_to_top.removeClass('toTop_visible toTop_fadeout');
+		if( $(this).scrollTop() > offset_opacity ) { 
+			$back_to_top.addClass('toTop_fadeout');
+		}
+	});
+	
+	
+	
+	//回调函数的作用是显示对应分页的列表项内容
+	//回调函数在用户每次点击分页链接的时候执行
+	//参数page_index{int整型}表示当前的索引页
+	var initPagination = function() {
+		var num_entries = $("#hiddenresult div.result").length;
+		// 创建分页
+		$("#Pagination").pagination(num_entries, {
+			num_edge_entries: 3, //边缘页数
+			num_display_entries: 3, //主体页数
+			callback: pageselectCallback,
+			items_per_page:1 ,//每页显示1项
+			prev_text:"上一页",
+			next_text:"下一页",
+		});
+	 }();
+	 
+	function pageselectCallback(page_index, jq){
+		var new_content = $("#hiddenresult div.result:eq("+page_index+")").clone();
+		$("#Searchresult").empty().append(new_content); //装载对应分页的内容
+		return false;
+	}
+	
+
+	
+});
+
+
+
+
+
+
+
+
---
+++ js/main.js
@@ -0,0 +1,76 @@
+﻿// JavaScript Document
+$(function () {
+	
+    //toTop返回顶部
+    	// browser window scroll (in pixels) after which the "back to top" link is shown
+	var offset = 300,
+		//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+		offset_opacity = 1200,
+		//duration of the top scrolling animation (in ms)
+		scroll_top_duration = 700,
+		//grab the "back to top" link
+		$back_to_top = $('.toTop');
+
+	//hide or show the "back to top" link
+	$(window).scroll(function(){
+		( $(this).scrollTop() > offset ) ? $back_to_top.addClass('toTop_visible') : $back_to_top.removeClass('toTop_visible toTop_fadeout');
+		if( $(this).scrollTop() > offset_opacity ) { 
+			$back_to_top.addClass('toTop_fadeout');
+		}
+	});
+
+	//smooth scroll to top
+	$back_to_top.on('click', function(event){
+		event.preventDefault();
+		$('body,html').animate({
+			scrollTop: 0 ,
+		 	}, scroll_top_duration
+		);
+	});
+	
+	
+    //新闻向上滚动
+    setInterval(function AutoScroll() {
+        $('#news_scroll').find("ul:first").animate({
+            marginTop: "-25px"
+        }, 1000, function () {
+            $(this).css({ marginTop: "0px" }).find("li:first").appendTo(this);
+        });
+    }, 3500)	
+		
+	
+	//内容页左边导航
+    $(".float_nav dl").click(function () {
+        $(this).addClass("selected")
+          .children("dd").show(1000).end()
+          .siblings().removeClass("selected")
+          .children("dd").hide(1000); 
+    });
+	
+	$(".float_nav dl dd a").click(function(){
+		$(this).addClass("a_selected").parent().siblings().find("a").removeClass("a_selected")
+		}
+	)
+	
+
+	
+	//百度统计
+	var _hmt = _hmt || [];
+        (function () {
+            var hm = document.createElement("script");
+            hm.src = "//hm.baidu.com/hm.js?b58d18764a096a7ea8f4120faebc95e1";
+            var s = document.getElementsByTagName("script")[0];
+            s.parentNode.insertBefore(hm, s);
+        })();
+	
+	
+	
+});
+
+
+
+
+
+
+
+
